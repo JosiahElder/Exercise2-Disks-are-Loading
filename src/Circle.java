d9217deed9bd88c506c5b05012922528ce545829
JosiahElder <@JosiahElder>
2023-02-01 13:09:02 -0800

    Next Commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,40 @@
-public class Circle {
+public class Circle
+{
+    private Double radius;
+    private String colour;
+
+    //Constructor
+    public Circle()
+    {
+        this.colour = "green";
+        this.radius = 2.0;
+    }
+
+    // Overload Constructor
+    public Circle(String colour, Double radius)
+    {
+        this.colour = colour;
+        this.radius = radius;
+    }
+
+    public double getArea()
+    {
+        return (3.14*radius*radius);
+    }
+
+    public String toString()
+    {
+        return ("Radius = " + getRadius() + " - Colour = " + getColour());
+    }
+
+    //Getters
+    public String getColour()
+    {
+        return colour;
+    }
+
+    public double getRadius()
+    {
+        return radius;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello 2 2 World!");
+        Circle C1 = new Circle();
+        System.out.println("Default Constructor: \n"
+                + "Colour: " + C1.getColour()
+                + "\nRaduis: " + C1.getRadius()
+                + "\nArea: " + C1.getArea()
+                + "\nString: " + C1.toString());
     }
 }
